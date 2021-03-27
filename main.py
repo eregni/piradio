4283d8bdd4d2f3608f748c5a5f5211da087ad225
Edoardo Regni <eregni@pm.me>
2021-03-27 20:07:30 +0100

    basic stuff. set up radio url list and save last selected station

--- main.py
+++ main.py
@@ -1,12 +1,42 @@
 #!/usr/bin/python3
-import time
 import vlc
-url = 'http://icecast.vrtcdn.be/klara-high.mp3'
+
+SAVED_STATION = 'last_station.txt'
+RADIO = [
+        'http://icecast.vrtcdn.be/klara-high.mp3',
+        'http://icecast.vrtcdn.be/ra2ant-high.mp3',
+        'http://icecast.vrtcdn.be/klara-high.mp3',
+        'http://icecast.vrtcdn.be/klaracontinuo-high.mp3',
+        'http://progressive-audio.lwc.vrtcdn.be/content/fixed/11_11niws-snip_hi.mp3'
+    ]
+
+
+def main():
+    player = vlc.MediaPlayer()
+    try:
+        with open(SAVED_STATION, 'r') as f:
+            station = f.readline()
+    except FileNotFoundError:
+        station = get_next_station()
+        save_last_station(station)
+    
+    player.set_mrl(station)
+    player.play()
+    while True:  # todo -> btn 'stop' || btn 'next' == False
+        pass
+
+
+def get_next_station(current_station=RADIO[-1]):
+    if current_station == RADIO[-1]:
+        return RADIO[0]
+    else:
+        return RADIO[RADIO.index(current_station) + 1]
+
+
+def save_last_station(station):
+    with open(SAVED_STATION, 'w') as f:
+        f.write(station)
+
 
 if __name__ == '__main__':
-    instance = vlc.Instance()
-    player = instance.media_player_new()
-    media = instance.media_new_location(url)
-    player.set_media(media)
-    player.play()
-    time.sleep(100)
+    main()
