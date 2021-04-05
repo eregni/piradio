d76b6e1362ecee682cee9ee4b2bb1f44585f5579
Edoardo Regni <eregni@pm.me>
2021-04-05 20:57:03 +0200

    implemented mpv module

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
 Script to play radio streams on a raspberrypi.
-Controlled by two buttons. 1 to play next station and 1 to stop playing
-I should be runned as a systemd service.
+Controlled by two buttons. 1 to play next station and 1 to start/stop playing
+It should be runned as a systemd service.
 For playing the media the script uses the python-mpv module to interact with mpv player.
-A small display is used to show the current played track
+A small display is used to show the current played track.
 Sources:
     radio stream url's: https://hendrikjansen.nl/henk/streaming1.html#wl
     python-mpv: https://github.com/jaseg/python-mpv
@@ -19,7 +19,7 @@ RADIO = (
         'http://icecast.vrtcdn.be/ra2ant.aac',
         'http://icecast.vrtcdn.be/klara.aac',
         'http://icecast.vrtcdn.be/klaracontinuo.aac',
-        'https://radios.rtbf.be/laprem1ere-128.mp3',
+        'https://radios.rtbf.be/laprem1ere-128.mp3',  # aac not available in 128 bit quality
         'https://radios.rtbf.be/musiq3-128.aac',
         'http://progressive-audio.lwc.vrtcdn.be/content/fixed/11_11niws-snip_hi.mp3'
 )
@@ -42,65 +42,79 @@ LOG.setLevel(LEVEL)
 
 
 def my_log(loglevel, component, message):
-    """Log handler for the python-mpv.MPV instance"""
+    """Log handler for the python-mpv.MPV instance. It just prints the log message"""
     print('[python-mpv] [{}] {}: {}'.format(loglevel, component, message))
 
 
 def main():
     """Main loop"""
-    LOG.debug("start radio")
+    btn_toggle = False
+    btn_next = False
+    LOG.info("start radio")
     player = mpv.MPV(log_handler=my_log)
-    player.set_loglevel('info')
+    player.set_loglevel('error')
 
-    try:
-        with open(SAVED_STATION, 'r') as f:
-            station = f.readline()
-    except FileNotFoundError:
-        station = get_next_station()
-        save_last_station(station)
+    index = get_saved_station()
     player.playlist_clear()
-
+    player.loop_playlist = True
     for url in RADIO:
         player.playlist_append(url)
 
-    player.playlist_play_index(0)
-    while True:  # todo -> btn 'stop' || btn 'next' == False. update metadate with interval
-        pass
+    player.playlist_play_index(index)
+    playing = True
+    current_playing = ""
+
+    while True:  # todo -> btn 'stop' || btn 'next' == False. update metadata with interval
+        if btn_toggle:
+            playing = not playing
+            if playing:
+                player.stop()
+            else:
+                player.playlist_play_index(player.playlist_current_pos)
+        if btn_next:
+            player.playlist_next()
+            save_last_station(player)
+        if current_playing != player.metadata['icy-title']:
+            update_metadata(player)
+            current_playing = player.metadata['icy-title']
 
 
-def get_next_station(current_station=RADIO[-1]):
+def update_metadata(player: mpv.MPV) -> None:
     """
-    Get next station from list
-    :param current_station: list with station url's
-    :return: str. Radio url
+    Send metadata to lcd screen
+    :param player: python-mpv MPV instance
     """
-    if current_station == RADIO[-1]:
-        radio = RADIO[0]
-    else:
-        radio = RADIO[RADIO.index(current_station) + 1]
-
-    LOG.info("Getting next station: {}".format(radio))
-    return radio
+    # todo: choose hardware...
+    name = player.metadata['icy-name']
+    title = player.metadata['icy-title']
+    LOG.debug("New metadata: {} {}".format(name, title))
 
 
-def get_radio_metadata(player):
+def get_saved_station() -> int:
     """
-    Get icy-data from radio station
-    :param player: python-mpv.MPV.
-    :return: dict with metadata
+    get saved playlist index nr
+    :return: int: index nr
     """
-    LOG.debug("Getting metadata")
-    return player.metadata
+    try:
+        with open(SAVED_STATION, 'r') as f:
+            index = int(f.readline())
+        LOG.debug("Retrieving saved index nr: {}".format(index))
+    except (FileNotFoundError, BaseException):
+        index = 0
+        LOG.warning("Error while reading saved playlist index nr. "
+                    "Getting first item in playlist instead")
+
+    return index
 
 
-def save_last_station(station):
+def save_last_station(player: mpv.MPV):
     """
-    Save radio url to file
-    :param station: str. radio stream url
+    Save station playlist index to file
+    :param player: python-mpv MPV instance
     """
     with open(SAVED_STATION, 'w') as f:
-        f.write(station)
-    LOG.debug("Saved station url to file")
+        f.write(str(player.playlist_current_pos))
+    LOG.debug("Saved station playlist index to file")
 
 
 def signal_exit_program(sig_nr, *args):
@@ -108,8 +122,7 @@ def signal_exit_program(sig_nr, *args):
     handler for SIGINT, SIGTERM, SIGHUP
     :param sig_nr: int
     """
-
-    LOG.debug("Signal received %s. Exit radio program", sig_nr)
+    LOG.info("Signal received %s. Exit radio program", sig_nr)
     SystemExit(0)
 
 
