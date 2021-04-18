6c8e7ef82b54cbd12f0411d5411e8facee151eed
Edoardo Regni <eregni@pm.me>
2021-04-18 21:16:19 +0200

    radio is playing

--- main.py
+++ main.py
@@ -1,46 +1,58 @@
 #!/usr/bin/python3
 """
-Script to play radio streams on a raspberrypi.
+Script to play radio streams on a raspberrypi pimped with an audiophonics sabre dac v3
 Controlled by two buttons. 1 to play next station and 1 to start/stop playing
 It should be runned as a systemd service.
-For playing the media the script uses the python-mpv module to interact with mpv player.
-A small display is used to show the current played track.
+A small display is used to show the current played track.   # todo: choose hardware...
 
 Usefull sources:
+    arch arm config: https://archlinuxarm.org/platforms/armv7/broadcom/raspberry-pi-2
+    run gpio as non-root: https://arcanesciencelab.wordpress.com/2016/03/31/running-rpi3-applications-that-use-gpio-without-being-root/
     radio stream url's: https://hendrikjansen.nl/henk/streaming1.html#wl
     python-mpv: https://github.com/jaseg/python-mpv
     audiophonics sabre dac v3: https://www.audiophonics.fr/en/index.php?controller=attachment&id_attachment=208
         THERE IS AN ERROR WITH THE NUMBERING OF THE PINS IN THE DAC DOCUMENTATION.
-        HALFWAY, IT SWITCHES FROM BCM TO PHYSICAL NUMBERING
+        HALFWAY, IT SWITCHES FROM BCM TO PHYSICAL PIN NUMBERING
         the dac occupies the following rpi pins (bcm numbering):
             4, 17, 22 (software shutdown, button shutdown, bootOk)
-            14, 15 (uart)
             18, 19, 21 (dac audio, DOCUMENTATION REFERS TO PHYSICAL PIN NRS 12, 35, 40)
-            pin 24, 25 for radio stream buttons (todo -> probably change to pin 14, 15
-            TODO: check these pins on the pcb for the lcd
+
+SET-UP
 The raspberrypi needs following packages (arch linux):
     mpv
     alsa-utils
     python-raspberry-gpio (from AUR -> yay is a usefull program to install aur packages)
 
-enable spi/i2c: "device_tree_param=spi=on"/"dtparam=i2c_arm=on" -> /boot/config.txt
-
-python modules:
+python modules (use pip to install):
     python-mpv
     gpiozero
+
+gpio permissions:
+    create file '99-gpio.rules' in /etc/udev/rules.d/ and add following config:
+    SUBSYSTEM=="bcm2835-gpiomem", KERNEL=="gpiomem", GROUP="gpio", MODE="0660"
+    SUBSYSTEM=="gpio", KERNEL=="gpiochip*", ACTION=="add", PROGRAM="/bin/sh -c 'chown root:gpio /sys/class/gpio/export /sys/class/gpio/unexport ; chmod 220 /sys/class/gpio/export /sys/class/gpio/unexport'"
+    SUBSYSTEM=="gpio", KERNEL=="gpio*", ACTION=="add", PROGRAM="/bin/sh -c 'chown root:gpio /sys%p/active_low /sys%p/direction /sys%p/edge /sys%p/value ; chmod 660 /sys%p/active_low /sys%p/direction /sys%p/edge /sys%p/value'"
+    add user to (new) group 'gpio'
+
+drivers:
+    enable spi/i2c: "device_tree_param=spi=on"/"dtparam=i2c_arm=on" -> /boot/config.txt
+    enable sabre dac: "dtoverlay=hifiberry-dac" -> /boot/config.txt
+
+the user needs to be in the 'audio' group
+todo: choose mpv volume -> volume=[0-100] (default 100) -> mpv.conf
 """
 import logging
 import signal
 import gpiozero
 import mpv
-
+AUDIO_DEVICE = 'alsa/default:CARD=sndrpihifiberry'
 RADIO = (
         'http://icecast.vrtcdn.be/radio1.aac',
         'http://icecast.vrtcdn.be/radio1_classics.aac',
         'http://icecast.vrtcdn.be/ra2ant.aac',
         'http://icecast.vrtcdn.be/klara.aac',
         'http://icecast.vrtcdn.be/klaracontinuo.aac',
-        'https://radios.rtbf.be/laprem1ere-128.mp3',  # aac not available in 128 bit quality
+        'https://radios.rtbf.be/laprem1ere-128.mp3',  # aac not available in 128 bit quality for now
         'https://radios.rtbf.be/musiq3-128.aac',
         'http://progressive-audio.lwc.vrtcdn.be/content/fixed/11_11niws-snip_hi.mp3'
 )
@@ -66,12 +78,13 @@ LOG.setLevel(LEVEL)
 
 def my_log(loglevel, component, message):
     """Log handler for the python-mpv.MPV instance. It just prints the log message"""
-    print('[python-mpv] [{}] {}: {}'.format(loglevel, component, message))
+    if LEVEL == logging.DEBUG:
+        print('[python-mpv] [{}] {}: {}'.format(loglevel, component, message))
 
 
 def main():
     """Main loop"""
-    player = mpv.MPV(log_handler=my_log)
+    player = mpv.MPV(log_handler=my_log, audio_device=AUDIO_DEVICE)
     player.set_loglevel('error')
 
     def btn_toggle_handler():
@@ -110,12 +123,12 @@ def main():
             LOG.debug("No (icy) metadata available")
             pass
 
+
 def update_metadata(player: mpv.MPV) -> None:
     """
     Send metadata to lcd screen
     :param player: python-mpv MPV instance
     """
-    # todo: choose hardware...
     name = player.metadata['icy-name']
     title = player.metadata['icy-title']
     if LEVEL == logging.DEBUG:
